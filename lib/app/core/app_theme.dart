c3d3c9b6e7182dc28425e33bca2b3984ddec1f6d
MuhammedAshrafSaleh <muhammedashrafsaleh@gmail.com>
2024-07-14 16:23:08 +0300

    Start App

---
+++ lib/app/core/app_theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static Color primaryLight = const Color(0xFF5D9CEC);
+  static Color whiteColor = const Color(0xFFFFFFFF);
+  static Color blackColor = const Color(0xFF383838);
+  static Color redColor = const Color(0xFFEC4B4B);
+  static Color greenColor = const Color(0xFF61E757);
+  static Color grey = const Color(0xFFC8C9CB);
+  static Color bodyColorWhite = const Color(0xFFDFECDB);
+
+  /// Dark Colors
+  static Color primaryDark = const Color(0xFF141922);
+  static Color bodyColorblack = const Color(0xFF060E1E);
+  static ThemeData lightTheme = ThemeData(
+    primaryColor: primaryLight,
+    scaffoldBackgroundColor: bodyColorWhite,
+    appBarTheme: AppBarTheme(
+      backgroundColor: primaryLight,
+    ),
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'app/core/app_theme.dart';
+import 'app/home/home_screen.dart';
+
+void main() {
+  runApp(const Todo());
+}
+
+class Todo extends StatelessWidget {
+  const Todo({super.key});
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName : (context) => const HomeScreen(),
+      },
+      theme: AppTheme.lightTheme,
+    );
+  }
+}
