ce760bcc6e7ccab7f363feef0a70998f8f45ad76
MuhammedAshrafSaleh <muhammedashrafsaleh@gmail.com>
2024-07-15 16:51:39 +0300

    Upload Porject

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app/core/app_theme.dart';
 import 'app/home/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const Todo());
 }
 
